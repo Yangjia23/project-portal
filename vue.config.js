50ab769ff3c4b79acbd33b86407b4865b2293543
yangjay <yj960203only@gmail.com>
2020-06-13 01:26:51 +0800

    fix: build error

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/portal/'
-      : '/',
-    chainWebpack: (config)=>{
-      config.resolve.alias
-        .set('@$', resolve('src'))
-        .set('assets',resolve('src/assets'))
-        .set('components',resolve('src/components'))
-    }
+      : '/'
 }
